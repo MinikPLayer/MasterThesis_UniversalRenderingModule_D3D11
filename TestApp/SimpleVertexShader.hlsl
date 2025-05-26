c709f366de4c64079bcce92e87f63fe1f7a3483f
Michał Tomecki <minexplaypl@gmail.com>
2025-05-26 13:12:28 +0200

    Added URMScene project.

--- TestApp/SimpleVertexShader.hlsl
+++ TestApp/SimpleVertexShader.hlsl
@@ -11,7 +11,7 @@ struct VS_INPUT
 {
     float3 pos : POSITION;
     float3 normal : NORMAL;
-    float4 textureUV : TEXCOORD;
+    float2 textureUV : TEXCOORD;
 };
 
 PS_INPUT main(VS_INPUT input)
@@ -19,9 +19,9 @@ PS_INPUT main(VS_INPUT input)
     PS_INPUT output;
     
     output.pos = mul(float4(input.pos, 1.0f), WorldViewProjection);
-    output.fragPosition = mul(float4(input.pos, 1.0f), WorldMatrix);
+    output.fragPosition = mul(float4(input.pos, 1.0f), WorldMatrix).xyz;
 
-    output.textureUV = input.textureUV;
+    output.textureUV = input.textureUV.xy;
     output.normal = mul(float4(input.normal, 1.0f), InverseWorldMatrix).xyz;
     return output;
 }
